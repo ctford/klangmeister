90cfd91c1bdb99ee5b47165e5ff522b8c26f7124
Chris Ford <christophertford@gmail.com>
2015-12-19 21:08:39 +0800

    Extract the action processing into its own namespace.

--- src/cljs/leipzig_live/core.cljs
+++ src/cljs/leipzig_live/core.cljs
@@ -1,44 +1,9 @@
 (ns leipzig-live.core
   (:require
-    [leipzig-live.music :as music]
+    [leipzig-live.processing] ; Import action definitions.
     [leipzig-live.views :as view]
-    [leipzig-live.instruments :as instrument]
-    [leipzig-live.actions :as action]
     [leipzig-live.framework :as framework]
-    [reagent.core :as reagent :refer [atom]]
-    [cljs.js :as cljs]))
-
-;; -------------------------
-;; Evaluation
-
-(defonce compiler-state (cljs/empty-state))
-(defn evaluate
-  [expr-str]
-  (cljs/eval-str
-    compiler-state
-    (str "(identity " expr-str ")")
-    nil
-    {:eval cljs/js-eval}
-    #(:value %)))
-
-;; -------------------------
-;; Behaviour
-
-(extend-protocol framework/Action
-  action/Refresh
-  (process [{expr-str :text} state]
-    (let [new-state (assoc-in state [:text] expr-str)]
-      (if-let [value (evaluate expr-str)]
-        (assoc-in new-state [:music] value)
-        new-state)))
-
-  action/Play
-  (process [_ {pitches :music :as original-state}]
-    (music/play-on! instrument/beep! (repeat 1) pitches)
-    original-state))
-
-;; -------------------------
-;; Wiring
+    [reagent.core :as reagent :refer [atom]]))
 
 (defn mount-root []
   (let [state-atom (atom {:music [100 120]
---
+++ src/cljs/leipzig_live/processing.cljs
@@ -0,0 +1,29 @@
+(ns leipzig-live.processing
+  (:require
+    [leipzig-live.music :as music]
+    [leipzig-live.actions :as action]
+    [leipzig-live.framework :as framework]
+    [cljs.js :as cljs]))
+
+(defonce compiler-state (cljs/empty-state))
+(defn evaluate
+  [expr-str]
+  (cljs/eval-str
+    compiler-state
+    (str "(identity " expr-str ")")
+    nil
+    {:eval cljs/js-eval}
+    #(:value %)))
+
+(extend-protocol framework/Action
+  action/Refresh
+  (process [{expr-str :text} state]
+    (let [new-state (assoc-in state [:text] expr-str)]
+      (if-let [value (evaluate expr-str)]
+        (assoc-in new-state [:music] value)
+        new-state)))
+
+  action/Play
+  (process [_ {pitches :music :as original-state}]
+    (music/play-on! instrument/beep! (repeat 1) pitches)
+    original-state))
