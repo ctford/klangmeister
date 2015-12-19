e7564e8288630fa910c41c16e9733784fa37bb21
Chris Ford <christophertford@gmail.com>
2015-12-19 11:50:39 +0000

    Add an instruments namespace.

--- src/cljs/leipzig_live/core.cljs
+++ src/cljs/leipzig_live/core.cljs
@@ -1,23 +1,11 @@
 (ns leipzig-live.core
   (:require
     [leipzig-live.music :as music]
+    [leipzig-live.instruments :as instrument]
     [leipzig-live.actions :as action]
+    [leipzig-live.framework :as framework]
     [reagent.core :as reagent :refer [atom]]
     [cljs.js :as cljs]))
-
-;; -------------------------
-;; Sound
-
-(defonce context (js/window.AudioContext.))
-(defn beep! [freq start dur]
-  (let [start (+ start (.-currentTime context))
-        stop (+ start dur)]
-    (doto (.createOscillator context)
-      (.connect (.-destination context))
-      (-> .-frequency .-value (set! freq))
-      (-> .-type (set! "square"))
-      (.start start)
-      (.stop stop))))
 
 ;; -------------------------
 ;; Evaluation
@@ -35,25 +23,18 @@
 ;; -------------------------
 ;; Behaviour
 
-(defprotocol Action
-  (process [this state]))
-
-
-(extend-protocol Action
-  actions/Refresh
+(extend-protocol framework/Action
+  action/Refresh
   (process [{expr-str :text} state]
     (let [new-state (assoc-in state [:text] expr-str)]
       (if-let [value (evaluate expr-str)]
         (assoc-in new-state [:music] value)
         new-state)))
 
-  actions/Play
+  action/Play
   (process [_ {pitches :music :as original-state}]
-    (music/play-on! beep! (repeat 1) pitches)
+    (music/play-on! instrument/beep! (repeat 1) pitches)
     original-state))
-
-(defn apply-action! [state-atom action]
-  (swap! state-atom (partial process action)))
 
 ;; -------------------------
 ;; Views
@@ -62,8 +43,8 @@
   [:div [:h1 "Welcome to Leipzig Live!"]
    [:div [:input {:type "text"
                   :value (:text state)
-                  :on-change #(-> % .-target .-value ->Refresh handle!)}]
-   [:button {:on-click (fn [_] (handle! (->Play)))} "Play!"]]
+                  :on-change #(-> % .-target .-value action/->Refresh handle!)}]
+   [:button {:on-click (fn [_] (handle! (action/->Play)))} "Play!"]]
    [:div
     (-> state :music print)]])
 
@@ -74,7 +55,7 @@
   (let [state-atom (atom {:music [100 120]
                           :text "'(100 120)"})]
     (reagent/render
-      [home-page (partial apply-action! state-atom) @state-atom]
+      [home-page (partial framework/apply-action! state-atom) @state-atom]
       js/document.body)))
 
 (defn init! []
---
+++ src/cljs/leipzig_live/instruments.cljs
@@ -0,0 +1,12 @@
+(ns leipzig-live.instruments)
+
+(defonce context (js/window.AudioContext.))
+(defn beep! [freq start dur]
+  (let [start (+ start (.-currentTime context))
+        stop (+ start dur)]
+    (doto (.createOscillator context)
+      (.connect (.-destination context))
+      (-> .-frequency .-value (set! freq))
+      (-> .-type (set! "square"))
+      (.start start)
+      (.stop stop))))
