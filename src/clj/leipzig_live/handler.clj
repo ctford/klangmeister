726140d60945a2e7bf3fd8248525fa200104cd8f
Chris Ford <christophertford@gmail.com>
2015-12-22 06:09:17 +0800

    Make links protocol relative.

--- src/clj/leipzig_live/handler.clj
+++ src/clj/leipzig_live/handler.clj
@@ -23,13 +23,13 @@
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1"}]
      (include-css
-       "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.8.0/codemirror.min.css"
+       "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.8.0/codemirror.min.css"
        (if (env :dev) "css/site.css" "css/site.min.css"))]
     [:body
      mount-target
      (include-js
-       "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.8.0/codemirror.min.js"
-       "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.8.0/mode/clojure/clojure.min.js"
+       "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.8.0/codemirror.min.js"
+       "//cdnjs.cloudflare.com/ajax/libs/codemirror/5.8.0/mode/clojure/clojure.min.js"
        "js/app.js")]]))
 
 
