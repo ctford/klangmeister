170b8587ff8568eb80382ebbb97c92d8e73a959f
Chris Ford <christophertford@gmail.com>
2015-12-21 08:58:06 +0800

    Document the framework.

--- src/cljs/leipzig_live/framework.cljs
+++ src/cljs/leipzig_live/framework.cljs
@@ -1,14 +1,19 @@
 (ns leipzig-live.framework)
 
 (defprotocol Action
+  "Calculate the new state and `handle!` any new Actions that arise."
   (process [this handle! state]))
 
 (declare handler-for)
 
-(defn apply-action! [state-atom action]
+(defn apply-action!
+  "Update the atom using the action."
+  [state-atom action]
   (swap!
     state-atom
     (fn [state] (process action (handler-for state-atom) state))))
 
-(defn handler-for [state-atom]
+(defn handler-for
+  "Build a `handle!` function for the atom."
+  [state-atom]
   (partial apply-action! state-atom))
