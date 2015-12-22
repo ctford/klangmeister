0a120f7947e4f2f25f73ac8bd2d4a9a893b6c579
Chris Ford <christophertford@gmail.com>
2015-12-23 05:43:53 +0800

    Shed unused dependencies.

--- project.clj
+++ project.clj
@@ -5,30 +5,15 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.reader "1.0.0-alpha1"]
-                 [reagent "0.5.1"
-                  :exclusions [org.clojure/tools.reader]]
+                 [reagent "0.5.1"]
                  [reagent-forms "0.5.13"]
                  [reagent-utils "0.1.5"]
-                 [prone "0.8.2"]
-                 [compojure "1.4.0"]
-                 [hiccup "1.0.5"]
-                 [environ "1.0.1"]
-                 [org.clojure/clojurescript "1.7.170" :scope "provided"]
-                 [secretary "1.2.3"]
-                 [venantius/accountant "0.1.5"
-                  :exclusions [org.clojure/tools.reader]]
-                 
-                 ]
+                 [org.clojure/clojurescript "1.7.170" :scope "provided"]]
 
-  :plugins [[lein-environ "1.0.1"]
-            [lein-cljsbuild "1.1.1"]
-            [lein-asset-minifier "0.2.2"
-             :exclusions [org.clojure/clojure]]]
+  :plugins [[lein-cljsbuild "1.1.1"]
+            [lein-asset-minifier "0.2.2" :exclusions [org.clojure/clojure]]]
 
   :min-lein-version "2.5.0"
-
-  :uberjar-name "leipzig-live.jar"
 
   :clean-targets ^{:protect false} [:target-path
                                     [:cljsbuild :builds :app :compiler :output-dir]
@@ -50,34 +35,15 @@
 
   :profiles {:dev {:repl-options {:init-ns leipzig-live.repl}
 
-                   :dependencies [[lein-figwheel "0.5.0-2"
-                                   :exclusions [org.clojure/core.memoize
-                                                org.clojure/clojure
-                                                org.ow2.asm/asm-all
-                                                org.clojure/data.priority-map
-                                                org.clojure/tools.reader
-                                                org.clojure/clojurescript
-                                                org.clojure/core.async
-                                                org.clojure/tools.analyzer.jvm]]
-                                  [org.clojure/clojurescript "1.7.170"
-                                   :exclusions [org.clojure/clojure org.clojure/tools.reader]]
+                   :dependencies [[lein-figwheel "0.5.0-2"]
+                                  [org.clojure/clojurescript "1.7.170"]
                                   [org.clojure/tools.nrepl "0.2.12"]
                                   [com.cemerick/piggieback "0.2.1"]
-                                                                    [pjstadig/humane-test-output "0.7.0"]
-                                  ]
+                                  [pjstadig/humane-test-output "0.7.0"]]
 
                    :source-paths ["env/dev/clj"]
-                   :plugins [[lein-figwheel "0.5.0-2"
-                              :exclusions [org.clojure/core.memoize
-                                           org.clojure/clojure
-                                           org.ow2.asm/asm-all
-                                           org.clojure/data.priority-map
-                                           org.clojure/tools.reader
-                                           org.clojure/clojurescript
-                                           org.clojure/core.async
-                                           org.clojure/tools.analyzer.jvm]]
-                             [org.clojure/clojurescript "1.7.170"]
-                             ]
+                   :plugins [[lein-figwheel "0.5.0-2"]
+                             [org.clojure/clojurescript "1.7.170"]]
 
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
@@ -85,8 +51,7 @@
                    :figwheel {:http-server-root "public"
                               :server-port 3449
                               :nrepl-port 7002
-                              :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
-                                                 ]
+                              :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
                               :css-dirs ["resources/public/css"]}
 
                    :env {:dev true}
@@ -94,12 +59,7 @@
                    :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
                                               :figwheel {:on-jsload "leipzig-live.core/reload!"}
                                               :compiler {:main "leipzig-live.dev"
-                                                         :source-map true}}
-
-
-                                        }
-
-                               }}
+                                                         :source-map true}}}}}
 
              :uberjar {:hooks [minify-assets.plugin/hooks]
                        :prep-tasks ["compile" ["cljsbuild" "once"]]
