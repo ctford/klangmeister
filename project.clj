0a5522594a28dc2854da9bb75c08c2d84b77af4c
Chris Ford <christophertford@gmail.com>
2015-12-22 13:55:47 +0800

    Don't use ring - we want this app to not need a server-side component.

--- project.clj
+++ project.clj
@@ -5,14 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [ring-server "0.4.0"]
                  [org.clojure/tools.reader "1.0.0-alpha1"]
                  [reagent "0.5.1"
                   :exclusions [org.clojure/tools.reader]]
                  [reagent-forms "0.5.13"]
                  [reagent-utils "0.1.5"]
-                 [ring "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]
                  [prone "0.8.2"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
@@ -29,14 +26,9 @@
             [lein-asset-minifier "0.2.2"
              :exclusions [org.clojure/clojure]]]
 
-  :ring {:handler leipzig-live.handler/app
-         :uberwar-name "leipzig-live.war"}
-
   :min-lein-version "2.5.0"
 
   :uberjar-name "leipzig-live.jar"
-
-  :main leipzig-live.server
 
   :clean-targets ^{:protect false} [:target-path
                                     [:cljsbuild :builds :app :compiler :output-dir]
@@ -50,19 +42,16 @@
    {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
 
   :cljsbuild {:builds {:app {:source-paths ["src/cljs" "src/cljc"]
-                             :compiler {:output-to "target/cljsbuild/public/js/app.js"
-                                        :output-dir "target/cljsbuild/public/js/out"
+                             :compiler {:output-to "resources/public/js/app.js"
+                                        :output-dir "resources/public/js/out"
                                         :asset-path   "js/out"
                                         :optimizations :none
                                         :pretty-print  true}}}}
 
   :profiles {:dev {:repl-options {:init-ns leipzig-live.repl}
 
-                   :dependencies [[ring/ring-mock "0.3.0"]
-                                  [ring/ring-devel "1.4.0"]
-                                  [lein-figwheel "0.5.0-2"
+                   :dependencies [[lein-figwheel "0.5.0-2"
                                    :exclusions [org.clojure/core.memoize
-                                                ring/ring-core
                                                 org.clojure/clojure
                                                 org.ow2.asm/asm-all
                                                 org.clojure/data.priority-map
@@ -80,7 +69,6 @@
                    :source-paths ["env/dev/clj"]
                    :plugins [[lein-figwheel "0.5.0-2"
                               :exclusions [org.clojure/core.memoize
-                                           ring/ring-core
                                            org.clojure/clojure
                                            org.ow2.asm/asm-all
                                            org.clojure/data.priority-map
@@ -99,8 +87,7 @@
                               :nrepl-port 7002
                               :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
                                                  ]
-                              :css-dirs ["resources/public/css"]
-                              :ring-handler leipzig-live.handler/app}
+                              :css-dirs ["resources/public/css"]}
 
                    :env {:dev true}
 
--- src/clj/leipzig_live/server.clj
+++
@@ -1,9 +0,0 @@
-(ns leipzig-live.server
-  (:require [leipzig-live.handler :refer [app]]
-            [environ.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]])
-  (:gen-class))
-
- (defn -main [& args]
-   (let [port (Integer/parseInt (or (env :port) "3000"))]
-     (run-jetty app {:port port :join? false})))
