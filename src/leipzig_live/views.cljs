227e7fd22a4b6c694c8a13ea417df705b0720634
Chris Ford <christophertford@gmail.com>
2015-12-25 12:48:43 +0800

    Sketch the notes. There's an issue with redrawing.

--- src/leipzig_live/views.cljs
+++ src/leipzig_live/views.cljs
@@ -5,29 +5,25 @@
     [quil.middleware :as middleware]
     [reagent.core :as reagent]))
 
-(quil/defsketch sketch
-  :setup (constantly [{:time 0 :pitch 440}
-                      {:time 1 :pitch 660}
-                      {:time 2 :pitch 880}
-                      {:time 3 :pitch 660}
-                      {:time 4 :pitch 440}])
-  :draw (fn [state]
-          (quil/background 255)
-          (doseq [{:keys [time pitch]} state]
-            (quil/ellipse
-              (-> time (* 100) (+ 17))
-              (-> pitch - (* 0.25) (+ 300))
-              30
-              30)))
-  :host "graph"
-  :no-start true
-  :middleware [middleware/fun-mode]
-  :size [600 300])
+(defn draw-graph [{:keys [music]}]
+  (quil/sketch :setup (constantly music)
+               :draw (fn [state]
+                       (quil/background 255)
+                       (doseq [{:keys [time pitch]} state]
+                         (quil/ellipse
+                           (-> time (* 100) (+ 17))
+                           (-> pitch - (* 0.25) (+ 300))
+                           30
+                           30)))
+               :host "graph"
+               :no-start true
+               :middleware [middleware/fun-mode]
+               :size [600 300]))
 
 (defn graph [handle! state]
   (reagent/create-class
     {:render (fn [] [:canvas#graph {:width 300 :height 300}])
-     :component-did-mount sketch}))
+     :component-did-update #(draw-graph state)}))
 
 (defn editor-did-mount [handle! _]
   (fn [this]
