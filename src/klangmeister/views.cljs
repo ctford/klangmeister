1a6baa8035c0f32bbf26fe803f34c95f9b23554e
Chris Ford <christophertford@gmail.com>
2015-12-26 19:58:55 +0800

    Add Google tags.

--- src/klangmeister/views.cljs
+++ src/klangmeister/views.cljs
@@ -57,12 +57,24 @@
                  [:button {:on-click #(handle! (action/->Play))} "Play"]
                  [:button {:on-click #(handle! (action/->Stop))} "Stop"])
         error (:error state)]
-    [:div
-     [:div {:class "graph"} [graph handle! state-atom]]
-     [:div {:class "controls"} button]
-     [:div {:class (if error "error" "")} [editor handle! state]]
-     [:a {:href "https://github.com/ctford/klangmeister"}
-      [:img {:style {:position "absolute" :top 0 :right 0 :border 0}
-             :src "https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67"
-             :alt "Fork me on GitHub"
-             :data-canonical-src "https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"}]]]))
+    [:body
+     [:div {:dangerouslySetInnerHTML
+            {:__html
+             "<!-- Google Tag Manager -->
+             <noscript><iframe src=\"//www.googletagmanager.com/ns.html?id=GTM-MF4GTC\"
+             height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+             <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+             '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+             })(window,document,'script','dataLayer','GTM-MF4GTC');</script>
+             <!-- End Google Tag Manager -->"}}]
+     [:div
+      [:div {:class "graph"} [graph handle! state-atom]]
+      [:div {:class "controls"} button]
+      [:div {:class (if error "error" "")} [editor handle! state]]
+      [:a {:href "https://github.com/ctford/klangmeister"}
+       [:img {:style {:position "absolute" :top 0 :right 0 :border 0}
+              :src "https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67"
+              :alt "Fork me on GitHub"
+              :data-canonical-src "https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"}]]]]))
