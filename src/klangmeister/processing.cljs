ab56c5c0aa48030ad5436ebbba9d70fadad9a096
Chris Ford <christophertford@gmail.com>
2015-12-29 09:24:07 +1100

    Still use the bell as a default.

--- src/klangmeister/processing.cljs
+++ src/klangmeister/processing.cljs
@@ -29,7 +29,7 @@
   (process [this handle! {:keys [music looping?] :as state}]
     (if looping?
       (let [start (Date.now)]
-        (music/play-on! instrument/fuzz! music)
+        (music/play-on! instrument/bell! music)
         (js/setTimeout #(handle! this) (music/duration music))
         (assoc state :sync start))
       (dissoc state :sync))))
