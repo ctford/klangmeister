85d29585954052fa76d1bef5e38b4d2ffaef9798
Chris Ford <christophertford@gmail.com>
2016-01-06 16:01:38 +0000

    Construct a scaling function and apply it to the notes separately.

--- src/klangmeister/graph.cljs
+++ src/klangmeister/graph.cljs
@@ -1,18 +1,22 @@
 (ns klangmeister.graph
   (:require
     [leipzig.melody :as melody]
+    [leipzig.scale :as scale]
+    [leipzig.temperament :as temperament]
     [quil.core :as quil :include-macros true]
     [quil.middleware :as middleware]
     [reagent.core :as reagent]))
 
-(defn scale [k to ms]
-  (let [maximum (->> ms (map k) (apply max))
-        minimum (->> ms (map k) (apply min))
-        range (- maximum minimum)]
-    (->> ms
-         (melody/where k #(- % minimum))
-         (melody/where k #(/ % range))
-         (melody/where k #(* % to)))))
+(defn scaler-for [to values]
+  (let [maximum (apply max values)
+        minimum (apply min values)
+        spread (- maximum minimum)]
+    (fn [x] (-> x (- minimum) (/ spread) (* to)))))
+
+(def guide-frequencies
+  (map (comp temperament/equal scale/C scale/major) (range -10 10)))
+
+(js/console.log (apply print-str guide-frequencies))
 
 (defn draw-graph [state-atom]
   (let [[height width] [150 800]
@@ -25,9 +29,14 @@
                                                 (melody/wherever
                                                   #(and looping? (<= (:time %) relative-time))
                                                   :played? (melody/is true)))
+                                    scale-pitch (scaler-for (- height dot-height) (map :pitch music))
+                                    scale-time (scaler-for (- width dot-width) (map :time music))
                                     scaled (->> marked
-                                                (scale :time (- width dot-width))
-                                                (scale :pitch (- height dot-height)))]
+                                                (melody/where :pitch scale-pitch)
+                                                (melody/where :time scale-time))]
+                                #_(doseq [freq (map scale-pitch guide-frequencies)]
+                                  (quil/stroke 200)
+                                  (quil/line 0 (* 0.5 freq) width (* 0.5 freq)))
                                 (doseq [{:keys [time pitch played?]} scaled]
                                   (let [colour (if played? 200 20)
                                         half (partial * 0.5)
