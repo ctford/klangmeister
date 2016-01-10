dde51d7328c432d58d3c304be902f1a61b613464
Chris Ford <christophertford@gmail.com>
2016-01-10 17:45:36 +0000

    Extract gain.

--- src/klangmeister/instruments.cljs
+++ src/klangmeister/instruments.cljs
@@ -2,11 +2,17 @@
 
 (defonce context (js/window.AudioContext.))
 
-(defn perc [at attack peak decay]
+(defn gain [at peak]
+  (let [node (.createGain context)]
+    (doto (.-gain node)
+      (.setValueAtTime peak at))
+    node))
+
+(defn perc [at attack decay]
   (let [node (.createGain context)]
     (doto (.-gain node)
       (.setValueAtTime 0 at)
-      (.linearRampToValueAtTime peak (+ at attack))
+      (.linearRampToValueAtTime 1.0 (+ at attack))
       (.linearRampToValueAtTime 0 (+ at attack decay)))
     node))
 
@@ -32,24 +38,25 @@
   (let [harmonic (fn [n proportion]
                    (doto (sin-osc (* n pitch))
                      (from time (+ time 1.5))
-                     (connect (perc time 0.01 (* 0.05 proportion) proportion)
+                     (connect (perc time 0.01 proportion)
+                              (gain time 0.01)
                               (.-destination context))))]
     (doseq [h [1.0 2.0 3.0 4.1 5.2]
             p [1.0 0.6 0.4 0.3 0.2]]
       (harmonic h p))))
 
 (defn fuzz! [{:keys [time duration pitch]}]
-  (let [envelope (perc time 0.1 0.8 0.5)]
+  (let [envelope (perc time 0.1 0.5)]
     (doto (saw pitch)
       (from time (+ time 1.5))
-      (connect envelope (.-destination context)))))
+      (connect envelope (gain time 0.3) (.-destination context)))))
 
 (defn buzz! [{:keys [time duration pitch]}]
   (let [freqs [pitch (* pitch 1.01) (* pitch 0.99)]
-        envelopes [(perc time 0.3 0.4 0.2)
-                   (perc time 0.05 0.3 0.1)
-                   (perc time 0.1 0.01 0.1)]]
+        envelopes [(perc time 0.3 0.2)
+                   (perc time 0.05 0.1)
+                   (perc time 0.1 0.1)]]
     (doseq [freq freqs envelope envelopes]
       (doto (saw freq)
         (from time (+ time 1.5))
-        (connect envelope (.-destination context))))))
+        (connect envelope (gain time 0.05) (.-destination context))))))
