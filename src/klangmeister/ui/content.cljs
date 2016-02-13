0ce0a8202767672b7a601de21998a7b0d197d507
Chris Ford <christophertford@gmail.com>
2016-02-13 19:29:28 +0000

    Revise copy after feedback.

--- src/klangmeister/ui/content.cljs
+++ src/klangmeister/ui/content.cljs
@@ -8,56 +8,58 @@
 
 (def steps
   {:sine
-   ["Sound is made up of pressure waves in the air. Here is the simplest possible synthesiser - a bare sine wave
-    with a frequency of 440 hertz."
+   ["Sound is made up of pressure waves in the air. Here is the simplest possible synthesiser - a sine wave
+    with a frequency of 440 hertz. When the button is pressed, the code in the box is evaluated to produce sound.
+    Anything after a \";\" is ignored, so we can include little notes alongside the code."
     "(sine 440) ; What happens if you raise the frequency?"]
     :gain
-   ["That's a bit loud. To control the volume of a sound, we alter the amplitude of the wave. To do that, we use something called a gain. A gain multiplies the amplitude by a specified factor. We use it by connecting our sine wave to it."
+   ["That's a bit loud. To control the volume of a sound, we alter the amplitude of the wave. To do that, we need something called a gain. A gain multiplies the amplitude by a specified factor. We use it by connecting our sine wave to it."
     "(connect->
   (sine 440)
   (gain 0.1)) ; Try adjusting the gain."]
    :interference
-   ["To prove sound is made from sine waves, let's add together two waves that have slightly different frequencies.
+   ["To show that sound is made from sine waves, let's add together two waves that have slightly different frequencies.
     As they fall in and out of phase, they alternately reinforce and cancel each other out."
     "(connect->
   (add
     (sine 440)
-    (sine 442)) ; What happens if the two frequencies are closer together? Why?
+    (sine 442)) ; What happens if the two frequencies are further apart? Why?
   (gain 0.1))"]
    :envelope
-   ["The sound cuts off too abruptly. In order to shape it into a real note, we need what's called an envelope. The simplest example of an envelope is a percussive envelope. A percussive envelope is defined by an attack - how long it takes the note to get to maximum loundness, and a decay - how long it takes the note to die away. We use the envelope by adding it to our chain of connected synthesis nodes."
+   ["The sound cuts off too abruptly. In order to shape it into a real note, we need what's called an envelope. The simplest example of an envelope is a percussive envelope. A percussive envelope is defined by an attack (how long it takes the note to get to maximum loundness) and a decay (how long it takes the note to die away). We use the envelope by adding it to our chain of connected synthesis nodes."
     "(connect->
   (add
     (sine 440)
     (sine 442))
-  (percussive 0.001 0.4) ; What does it sound like if you reverse the two numbers?
+  (percussive 0.001 0.4) ; What does it sound like if you reverse attack and decay?
   (gain 0.1))"]
    :oscillators
-   ["Sine waves are the simplest oscillators, but there are periodic waves that also produce sound.  "
+   ["Sine waves are the simplest oscillators, but there are other periodic waves that also produce sound. Examples include
+    square waves and sawtooth waves."
     "(connect->
-  (add
-    (square 440)
-    (triangle 442)) ; Try a sawtooth wave.
+  (square 440) ; Try a sawtooth wave.
   (percussive 0.001 0.4)
   (gain 0.1))"]
    :filters
-   ["Complicated waves like triangle waves can also be thought of as the sum of a series of sine waves that get higher and higher.
-    Because of that, they can be shaped by frequency filters. This one blocks all frequencies above 600 hertz."
+   ["Non-sinusoidal waves like squares can be expressed as the sum of a series of sine waves.
+    The mathematical details don't concern
+    us at the moment, but we can take advantage of the fact by subtly shaping sounds by filtering out some frequencies.
+    One kind of frequency filter is the high-pass filter, which removes frequencies below a cutoff. A low-pass filter does the opposite, and removes frequencies above a cutoff."
     "(connect->
   (add
     (square 440)
-    (triangle 442))
-  (low-pass 600) ; What happens to the sound if you raise or lower the cutoff?
+    (sawtooth 442))
+  (low-pass 600) ; What changes if you raise the cutoff frequency to 2600?
   (percussive 0.001 0.4)
   (gain 0.1))"]
    :sustain
-   ["Many instruments can sustain a note over a period of time. To achive that, we use an ADSR envelope instead of a percussive one. As well as an initial attack and decay (AD), an ADSR envelope holds the sound at a sustain level (S) before dying away over the release (R)."
+   ["Many instruments can sustain a note over a period of time. To achieve that, we use an ADSR envelope instead of a percussive one. As well as an initial attack and decay (AD), an ADSR envelope holds the sound at a sustain level (S) before dying away over the release (R)."
     "(connect->
   (add
     (square 440)
-    (triangle 442))
+    (sawtooth 442))
   (low-pass 600)
-  (adsr 0.001 0.4 0.5 0.1) ; Experiment with different values.
+  (adsr 0.001 0.4 0.5 0.1) ; How does a longer attack sound?
   (gain 0.1))"]})
 
 (defn render-one [k handle! state-atom]
