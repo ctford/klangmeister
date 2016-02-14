6e1fea4e58cd47cec99ddc0ab898a0947b843464
Chris Ford <christophertford@gmail.com>
2016-02-14 22:43:26 +0000

    Attempt to be compatible with Safari.

--- src/klangmeister/sound/music.cljs
+++ src/klangmeister/sound/music.cljs
@@ -2,7 +2,7 @@
   (:require [leipzig.temperament :as temperament]
             [klangmeister.sound.synthesis :as synthesis]))
 
-(defonce context (js/window.AudioContext.))
+(defonce context (or (js/window.AudioContext.) (js/window.webkitAudioContext.)))
 
 (defn play! [notes]
   (doseq [{:keys [time duration instrument] :as note} notes]
