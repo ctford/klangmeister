a9461aee20ca4824fd85edebf37e8756a9673c7a
Chris Ford <christophertford@gmail.com>
2016-02-14 16:38:49 +0000

    Add more detail to the music tutorial.

--- src/klangmeister/ui/music_tutorial.cljs
+++ src/klangmeister/ui/music_tutorial.cljs
@@ -13,7 +13,7 @@
     "(phrase [3/3 3/3 2/3 1/3 3/3] ; You can change both the durations and the pitches.
         [ 72  72  72  74  76])"]
    :then
-   ["We can chain together two melodies to make a longer one."
+   ["We can combine two melodies to make a longer one."
     "(->>
   (phrase [3/3 3/3 2/3 1/3 3/3] ; Can you recognise the tune yet?
           [ 72  72  72  74  76])
@@ -21,8 +21,9 @@
     (phrase [2/3 1/3 2/3 1/3 3/3]
             [ 76  74  76  77  79])))"]
    :with
-   ["Alternatively, we can play two melodies at the same time. If they're well-chosen, we'll get harmony."
-   "(->>
+   ["Alternatively, we can play two melodies at the same time. If they're well-chosen, we'll get harmony. If not, we might
+    still get something interesting."
+   "(->> ; The arrow combines melodies like connect-> combines synthesiser nodes.
   (phrase [3/3 3/3 2/3 1/3 3/3]
           [ 72  72  72  74  76])
   (with
@@ -36,10 +37,11 @@
   (with
     (phrase [2/3 1/3 2/3 1/3 3/3]
             [ 76  74  76  77  79]))
-  (times 2))"]
+  (times 2)) ; What happens if the times is put before the with? Why?"]
    :bpm
-   ["So far every unit of time has been a second. If we want to speed up or slow down the music, we can define how many
-    beats-per-minute there are."
+   ["So far every unit of time has been a second, in other words 60 beats per minute.
+    If we want to speed up or slow down the music, we can define how many beats-per-minute there are. We use 'tempo' to
+    update the time and duration of each note according to the chosen 'bpm'."
     "(->>
   (phrase [3/3 3/3 2/3 1/3 3/3]
           [ 72  72  72  74  76])
@@ -49,7 +51,7 @@
   (times 2)
   (tempo (bpm 90))) ; Try making the music faster or slower."]
    :key
-   ["We have to be careful which pitches we choose. A given piece of music focuses on certain pitches and ignores others. For example, 72 and 74 are in C major, but 73 is not. A convenient way to write songs in-key is to number the notes within the key and then later use code to translate those numbers into specific pitches. Here we use the where function to put each note in C major."
+   ["We have to be careful which pitches we choose if we want the result to sound musical. A given piece of music focuses on certain pitches and ignores others, determined by the key of the piece. For example, 72 and 74 are in C major, but 73 is not. A convenient way to write songs in-key is to number the notes within the key and then later use code to translate those numbers into specific pitches. Here we use 'where' to put each note in C major."
     "(->>
   (phrase [3/3 3/3 2/3 1/3 3/3]
           [  0   0   0   1   2])
