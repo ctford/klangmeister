7833c425e59b192c316091204f3b5d7fc019d6f8
Chris Ford <christophertford@gmail.com>
2016-02-21 16:11:10 +0000

    Add hat and kick percussive sounds.

--- src/klangmeister/sound/instruments.cljs
+++ src/klangmeister/sound/instruments.cljs
@@ -1,6 +1,8 @@
 (ns klangmeister.sound.instruments
   (:require
-    [klangmeister.sound.synthesis :refer [connect-> percussive sine add gain]]))
+    [klangmeister.sound.synthesis :refer
+     [connect-> percussive sine add gain high-pass low-pass white-noise
+      triangle constant envelope]]))
 
 (defn bell
   "An imitation bell, made by adding together harmonics."
@@ -13,3 +15,27 @@
       (->> (map harmonic [1.0 2.0 3.0 4.1 5.2]       ; Each harmonic is a multiple of the base frequency.
                          [1.0 0.6 0.4 0.3 0.2])      ; Higher harmonics are weaker.
            (apply add))))
+
+(defn high-hat [decay]
+  (fn [_]
+    (connect->
+      white-noise
+      (percussive 0.01 0.4)
+      (high-pass 3000)
+      (low-pass 4500)
+      (gain 0.2))))
+
+(def open-hat 0.4)
+(def closed-hat 0.1)
+
+(defn tom [pitch decay]
+  (fn [_]
+    (connect->
+      (add
+        (sine pitch)
+        (triangle (connect-> (constant pitch) (envelope [0 1] [0.5 0.5]))))
+      (low-pass (* 3 pitch))
+      (percussive 0.01 decay)
+      (gain 0.2))))
+
+(def kick (tom 50 0.2))
