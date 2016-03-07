5ec3b1d4b2e900d95ee949c5768292dc1a5f0606
Chris Ford <christophertford@gmail.com>
2016-03-07 22:01:47 +0000

    Rearrange and comment.

--- src/klangmeister/core.cljs
+++ src/klangmeister/core.cljs
@@ -9,15 +9,14 @@
     [accountant.core :as accountant]
     [secretary.core :as secretary :include-macros true]))
 
-(defn empty-state []
-  (reagent/atom
-    {:audiocontext (if js/window.AudioContext.
-                     (js/window.AudioContext.)
-                     (js/window.webkitAudioContext.))}))
+(defn audio-context
+  "Construct an audio context in a way that works even if it's prefixed."
+  []
+  (if js/window.AudioContext. ; Some browsers e.g. Safari don't use the unprefixed version yet.
+    (js/window.AudioContext.)
+    (js/window.webkitAudioContext.)))
 
-(defonce state-atom (empty-state))
-
-(def handle! (framework/handler-for state-atom))
+(defonce state-atom (reagent/atom {:audiocontext (audio-context)}))
 
 (secretary/defroute "/klangmeister/"            [query-params] (session/put! :gist (:gist query-params))
                                                                (session/put! :current-page view/about))
@@ -31,19 +30,20 @@
 (secretary/defroute "/klangmeister/reference"   [] (session/put! :current-page view/reference))
 (secretary/defroute "/klangmeister/about"       [] (session/put! :current-page view/about))
 
-(defn current-page []
+(def handle!
+  "An handler that components can use to raise events."
+  (framework/handler-for state-atom))
+
+(defn current-page
+  "Extract the current page from the session and use it to build the page."
+  []
   [(session/get :current-page) handle! state-atom])
 
 (defn mount-root []
-  (let [default (or (session/get :gist) "4b04fd7f2d361c6604c4")]
-    (handle! (action/->Import default :main))
-    (reagent/render
-      [current-page]
-      js/document.body)))
-
-(defn main []
   (accountant/configure-navigation!)
   (accountant/dispatch-current!)
-  (mount-root))
+  (let [default (or (session/get :gist) "4b04fd7f2d361c6604c4")]
+    (handle! (action/->Import default :main)) ; Pull in the content of the main code pane.
+    (reagent/render [current-page] js/document.body)))
 
-(main)
+(mount-root)
