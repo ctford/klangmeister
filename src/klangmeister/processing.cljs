0645c5b1b25cb1b92a88aa810e57dd6e6706dad0
Chris Ford <christophertford@gmail.com>
2016-04-11 23:22:39 +0100

    Revert "Limit the looped notes to 200 to prevent infinite seq crashes."
    This reverts commit acc273864fcd4d202b01465813ccaa5d72eb9d4f.
    
    This broke the synthesis page.

--- src/klangmeister/processing.cljs
+++ src/klangmeister/processing.cljs
@@ -8,21 +8,15 @@
     [ajax.core :as ajax]
     [leipzig.melody :as melody]))
 
-(defn safety-cutoff
-  "Cutoff evaluation to prevent infinite seqs breaking everything."
-  [notes]
-  (let [max-notes 200]
-    (->> notes (take max-notes))))
-
 (extend-protocol framework/Action
   action/Refresh
   (process [{expr-str :text pane :target} _ state]
     (let [{:keys [value error]} (eval/uate expr-str)
-          value (some-> value safety-cutoff)]
+          value (or value (get-in state [pane :value]) )]
       (-> state
           (assoc-in [pane :error] error)
           (assoc-in [pane :text] expr-str)
-          (update-in [pane :value] #(or value %)))))
+          (assoc-in [pane :value] value))))
 
   action/Import
   (process [{gist :gist pane :target} handle! state]
