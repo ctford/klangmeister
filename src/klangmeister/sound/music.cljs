6f89f653b19657017141e534b86e7d064684c4b4
Chris Ford <christophertford@gmail.com>
2017-11-14 22:16:25 +0000

    Fix order of declaration.

--- src/klangmeister/sound/music.cljs
+++ src/klangmeister/sound/music.cljs
@@ -2,11 +2,6 @@
   (:require [leipzig.temperament :as temperament]
             [cljs-bach.synthesis :as synthesis]))
 
-(defn play!
-  "Take a sequence of notes and play them in an audiocontext."
-  [audiocontext notes]
-  (play-from! audiocontext (synthesis/current-time audiocontext) notes))
-
 (defn play-from!
   "Take a sequence of notes and play them from a set point in an audiocontext."
   [audiocontext from notes]
@@ -18,3 +13,8 @@
                              instrument)
           connected-instance (synthesis/connect synth-instance synthesis/destination)]
       (connected-instance audiocontext at duration))))
+
+(defn play!
+  "Take a sequence of notes and play them in an audiocontext."
+  [audiocontext notes]
+  (play-from! audiocontext (synthesis/current-time audiocontext) notes))
